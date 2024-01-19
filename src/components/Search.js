5250b14ec0c9dbe81d78a219cb7f504f273d12aa
Soham <113625563+Soham-Chatt@users.noreply.github.com>
2024-01-19 16:01:59 +0100

    Added bootstrap icons package.

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 function Search({ showVisited, visitedCount, hallCount, onSearchChange }) {
   const handleSearchInput = (event) => {
